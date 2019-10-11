fb48e54f1fec3d0c4c6d1332b6f9a2f77795590d
cejaythakkar <ce.jaythakkar@gmail.com>
2019-10-11 17:48:12 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import classes from "./App.module.scss";
 
 function App() {
+  console.log(classes);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={classes.app}>
+      <header className={classes.appHeader}>
+        <img src={logo} className={classes.appLogo} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={classes.appLink}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
